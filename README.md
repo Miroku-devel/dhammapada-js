8f22632db0064c310960eade856781dae3aaacd7
Andrea <194716967+Miroku-devel@users.noreply.github.com>
2026-06-27 14:44:51 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+  <img src="assets/icon.png" width="120">
+
+  <h1 style="font-size: 2em; margin: 10px 0;">Dhammapada</h1>
+  <h3 style="font-size: 1em;">A multilingual Dhammapada reader for the web.</h3>
+
+  <a href="https://miroku-devel.github.io/dhammapada-js/">
+    <img src="https://img.shields.io/badge/Live-Demo-blue" alt="Live Demo">
+  </a>
+</div>
+
+## Description
+
+This web application provides access to the Dhammapada, one of the fundamental works of the Buddhist tradition. It contains 423 verses of teachings attributed to the Buddha, presented in an accessible format in over 50 languages, allowing readers around the world to study the text in their own language.
+
+## Copyright and License
+The application logic, UI design, assets, and build scripts are licensed under the **GNU GPL 3.0**.
+
+All translations are the property of their respective authors or original publishers.
+For the complete list of translators, sources, and individual license terms for each language
+please consult the [CREDITS](./CREDITS) file.
---
+++ lang/index.js
@@ -0,0 +1,268 @@
+window.__dh_index = {
+  "chapterEnds": [
+    20,
+    32,
+    43,
+    59,
+    75,
+    89,
+    99,
+    115,
+    128,
+    145,
+    156,
+    166,
+    178,
+    196,
+    208,
+    220,
+    234,
+    255,
+    272,
+    289,
+    305,
+    319,
+    333,
+    359,
+    382,
+    423
+  ],
+  "chapterNames": [
+    "chap1",
+    "chap2",
+    "chap3",
+    "chap4",
+    "chap5",
+    "chap6",
+    "chap7",
+    "chap8",
+    "chap9",
+    "chap10",
+    "chap11",
+    "chap12",
+    "chap13",
+    "chap14",
+    "chap15",
+    "chap16",
+    "chap17",
+    "chap18",
+    "chap19",
+    "chap20",
+    "chap21",
+    "chap22",
+    "chap23",
+    "chap24",
+    "chap25",
+    "chap26"
+  ],
+  "languages": [
+    {
+      "code": "af",
+      "name": "Afrikaans"
+    },
+    {
+      "code": "ar",
+      "name": "العربية"
+    },
+    {
+      "code": "az",
+      "name": "Azərbaycanca"
+    },
+    {
+      "code": "bg",
+      "name": "Български"
+    },
+    {
+      "code": "bn",
+      "name": "বাংলা"
+    },
+    {
+      "code": "ca",
+      "name": "Català"
+    },
+    {
+      "code": "cs",
+      "name": "Čeština"
+    },
+    {
+      "code": "da",
+      "name": "Dansk"
+    },
+    {
+      "code": "de",
+      "name": "Deutsch"
+    },
+    {
+      "code": "el",
+      "name": "Ελληνικά"
+    },
+    {
+      "code": "en",
+      "name": "English"
+    },
+    {
+      "code": "es",
+      "name": "Español"
+    },
+    {
+      "code": "et",
+      "name": "Eesti"
+    },
+    {
+      "code": "fi",
+      "name": "Suomi"
+    },
+    {
+      "code": "fr",
+      "name": "Français"
+    },
+    {
+      "code": "he",
+      "name": "עברית"
+    },
+    {
+      "code": "hi",
+      "name": "हिन्दी"
+    },
+    {
+      "code": "hu",
+      "name": "Magyar"
+    },
+    {
+      "code": "id",
+      "name": "Bahasa Indonesia"
+    },
+    {
+      "code": "it",
+      "name": "Italiano"
+    },
+    {
+      "code": "ja",
+      "name": "日本語"
+    },
+    {
+      "code": "jv",
+      "name": "Jawa"
+    },
+    {
+      "code": "kk",
+      "name": "Қазақша"
+    },
+    {
+      "code": "km",
+      "name": "ភាសាខ្មែរ"
+    },
+    {
+      "code": "ko",
+      "name": "한국어"
+    },
+    {
+      "code": "ku",
+      "name": "Kurdî"
+    },
+    {
+      "code": "lo",
+      "name": "ລາວ"
+    },
+    {
+      "code": "lt",
+      "name": "Lietuvių"
+    },
+    {
+      "code": "ml",
+      "name": "മലയാളം"
+    },
+    {
+      "code": "mr",
+      "name": "मराठी"
+    },
+    {
+      "code": "ms",
+      "name": "Bahasa Melayu"
+    },
+    {
+      "code": "my",
+      "name": "မြန်မာဘာသာ"
+    },
+    {
+      "code": "nb",
+      "name": "Norsk Bokmål"
+    },
+    {
+      "code": "ne",
+      "name": "नेपाली"
+    },
+    {
+      "code": "nl",
+      "name": "Nederlands"
+    },
+    {
+      "code": "pa",
+      "name": "ਪੰਜਾਬੀ"
+    },
+    {
+      "code": "pl",
+      "name": "Polski"
+    },
+    {
+      "code": "pt",
+      "name": "Português"
+    },
+    {
+      "code": "ro",
+      "name": "Română"
+    },
+    {
+      "code": "ru",
+      "name": "Русский"
+    },
+    {
+      "code": "si",
+      "name": "සිංහල"
+    },
+    {
+      "code": "sl",
+      "name": "Slovenščina"
+    },
+    {
+      "code": "sq",
+      "name": "Shqip"
+    },
+    {
+      "code": "sr",
+      "name": "Српски"
+    },
+    {
+      "code": "sv",
+      "name": "Svenska"
+    },
+    {
+      "code": "ta",
+      "name": "தமிழ்"
+    },
+    {
+      "code": "te",
+      "name": "తెలుగు"
+    },
+    {
+      "code": "th",
+      "name": "ไทย"
+    },
+    {
+      "code": "tr",
+      "name": "Türkçe"
+    },
+    {
+      "code": "uk",
+      "name": "Українська"
+    },
+    {
+      "code": "vi",
+      "name": "Tiếng Việt"
+    },
+    {
+      "code": "zh",
+      "name": "中文"
+    }
+  ]
+};
